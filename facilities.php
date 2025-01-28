f9f01c1e2017fa3b09b4a0e4ed486dec9135803c
raisa <jannatur.cse.202201040144@aust.edu>
2025-01-28 13:20:00 +0600

    inc , About, Contact, facilities

---
+++ facilities.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hostel Management - facilities</title>
+    
+    <?php require('inc/links.php')?>
+    
+ <style>
+  .pop:hover{
+    border-top-color:var(--teal) !important;
+    transform: scale(1.03);
+    transition: all 0.3s;
+  }
+ </style>
+</head>
+<body class="bg-light">
+
+  <?php require('inc/header.php') ?>
+
+  <div class="my-5 px-4">
+    <h2 class="fw-blod h-font text-center">Our Facilities</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Obcaecati repellat maiores similique molestiae itaque unde atque laudantium neque! 
+       Adipisci deleniti reprehenderit iure esse nostrum laudantium, sed ea iste voluptates cum.
+    </p>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-6 md-5 px-4">
+        <div class="bg-white roundrd shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2"></div>
+          <img src="images/features//wifi-logo.svg" width="40px">
+          <h5 class="m-0 ">Wifi</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Praesentium quo, nihil temporibus earum non doloribus assumenda?
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 md-5 px-4">
+        <div class="bg-white roundrd shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2"></div>
+          <img src="images/features//wifi-logo.svg" width="40px">
+          <h5 class="m-0 ">Wifi</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Praesentium quo, nihil temporibus earum non doloribus assumenda?
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 md-5 px-4">
+        <div class="bg-white roundrd shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2"></div>
+          <img src="images/features//wifi-logo.svg" width="40px">
+          <h5 class="m-0 ">Wifi</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Praesentium quo, nihil temporibus earum non doloribus assumenda?
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 md-5 px-4">
+        <div class="bg-white roundrd shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2"></div>
+          <img src="images/features//wifi-logo.svg" width="40px">
+          <h5 class="m-0 ">Wifi</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Praesentium quo, nihil temporibus earum non doloribus assumenda?
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 md-5 px-4">
+        <div class="bg-white roundrd shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2"></div>
+          <img src="images/features//wifi-logo.svg" width="40px">
+          <h5 class="m-0 ">Wifi</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Praesentium quo, nihil temporibus earum non doloribus assumenda?
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 md-5 px-4">
+        <div class="bg-white roundrd shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2"></div>
+          <img src="images/features//wifi-logo.svg" width="40px">
+          <h5 class="m-0 ">Wifi</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Praesentium quo, nihil temporibus earum non doloribus assumenda?
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php require('inc/footer.php')?>
+
+
+
+</body>
+</html>
