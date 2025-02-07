22f282e7553273822c96a1e70989d1d1ab219ab1
Sara <malihatasnim530@gmail.com>
2025-02-07 16:42:16 +0600

    settings-part-1

--- admin/ajax/settings_crud.php
+++ admin/ajax/settings_crud.php
@@ -2,13 +2,13 @@
    
    require('../inc/db_config.php');
    require('../inc/essentials.php');
-   adminLogin(); //if not login,redirect to login page
+   //adminLogin(); //if not login,redirect to login page
 
 
    if(isset($_POST['get_general'])){ //checks if get_general has post data,if yes then retrieve the data
-    $q="select * from `settings` where `sr_no`=?";
+    $q="SELECT * from `settings` where `sr_no`=?";
     $values=[1];
-    $res=select($q,$values,"i");  //matcher the value with select
+    $res=select($q,$values,"i");  //matches the value with select
     $data=mysqli_fetch_assoc($res); //retrieves the result as an associative array.
     $json_data=json_encode($data);
     echo $json_data;
@@ -19,6 +19,7 @@
       $q="UPDATE `settings` SET `site_title`=?, `site_about`=? WHERE `sr_no`=?";
       $values=[$frm_data['site_title'],$frm_data['site_about'],1];
       $res=update($q,$values,"ssi");
+      echo $res;
    }
 
    if(isset($_POST['upd_shutdown'])){
@@ -26,6 +27,26 @@
       $q="UPDATE `settings` SET `shutdown`=? WHERE `sr_no`=?";
       $values=[$frm_data,1];
       $res=update($q,$values,"ii");
+      echo $res;
    }
+
+   if(isset($_POST['get_contacts'])){
+      $q="SELECT * from `contact_details` where `sr_no`=?";
+      $values=[1];
+      $res=select($q,$values,"i");
+      $data=mysqli_fetch_assoc($res);
+      $json_data=json_encode($data);
+      echo $json_data;
+     }
+
+     if(isset($_POST['upd_contacts'])){
+      $frm_data=filteration($_POST);
+      $q="UPDATE `contact_details` SET `address`=?,`gmap`=?,`pn1`=?,`email`=?,`fb`=?,`insta`=?,`tw`=?,`iframe`=? where `sr_no`=?";
+      $values=[$frm_data['address'],$frm_data['gmap'],$frm_data['pn1'],$frm_data['email'],$frm_data['fb'],$frm_data['insta'],$frm_data['tw'],$frm_data['iframe'],1];
+      $res=update($q,$values,"ssssssssi");
+      echo $res;
+   }
+
+
 
 ?>
--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -5,16 +5,15 @@ function adminLogin(){
   session_start();
   //if admin is not set and not true
   if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
-    echo"
-    <script>window.location.href='index.php'</script>
-    ";
+    echo"<script>window.location.href='index.php'</script>";
   }
-  session_regenerate_id(true);  //for generating new id
+exit;
+
+
 }
 function redirect($url){
-    echo"
-    <script>window.location.href='$url'</script>
-    ";
+    echo"<script>window.location.href='$url'</script>";
+    exit;
 } 
 
 function alert($type,$msg){
